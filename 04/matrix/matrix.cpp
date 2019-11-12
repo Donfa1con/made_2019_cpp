fba02e67d9e28101555c28fd46f41c97052d04cc
Dmitry Ziganshin <diziganshin@inbox.ru>
2019-11-12 10:02:05 +0300

    Add hw4

---
+++ 04/matrix/matrix.cpp
@@ -0,0 +1,70 @@
+#include "matrix.hpp"
+
+Matrix::Matrix(size_t rows, size_t columns): nrows(rows), ncols(columns), data(new int[nrows * ncols]) {};
+
+Matrix::~Matrix() {
+    delete[] data;
+}
+
+size_t Matrix::get_rows() const {
+    return nrows;
+}
+
+size_t Matrix::get_columns() const {
+    return ncols;
+}
+
+bool Matrix::operator==(const Matrix& other) const {
+    if (this == &other) {
+        return true;
+    }
+    if (nrows != other.nrows || ncols != other.ncols) {
+        return false;
+    }
+    for (int i = 0; i < nrows * ncols; ++i) {
+        if (data[i] != other.data[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool Matrix::operator!=(const Matrix& other) const{
+    return !(*this == other);
+}
+
+Matrix& Matrix::operator*=(int value) {
+    for (int i = 0; i < nrows * ncols; ++i) {
+        data[i] *= value;
+    }
+    return *this;
+}
+
+const Matrix::Row& Matrix::operator[](size_t row) const {
+    return Row(row, *this);
+}
+
+Matrix::Row& Matrix::operator[](size_t row) {
+    Row ret_row = Row(row, *this);
+    return ret_row;
+}
+
+const int& Matrix::Row::operator[](size_t col) const {
+    if (row >= matrix.nrows) {
+        throw std::out_of_range("Row index is out of range");
+    }
+    if (col >= matrix.ncols) {
+        throw std::out_of_range("Column index is out of range");
+    }
+    return matrix.data[row * matrix.ncols + col];
+}
+
+int& Matrix::Row::operator[](size_t col) {
+    if (row >= matrix.nrows) {
+        throw std::out_of_range("Row index is out of range");
+    }
+    if (col >= matrix.ncols) {
+        throw std::out_of_range("Column index is out of range");
+    }
+    return matrix.data[row * matrix.ncols + col];
+}
---
+++ 04/test.cpp
@@ -0,0 +1,32 @@
+#include <cassert>
+#include <iostream>
+
+#include "matrix/matrix.hpp"
+
+int main(int argc, const char * argv[]) {
+    const size_t rows = 5;
+    const size_t cols = 4;
+    Matrix m1(rows, cols);
+    Matrix m2(rows, cols);
+    for (int i = 0; i < m1.get_rows(); ++i) {
+        for (int j = 0; j < m1.get_columns(); ++j) {
+            m1[i][j] = i * j;
+        }
+    }
+    for (int i = 0; i < m2.get_rows(); ++i) {
+        for (int j = 0; j < m2.get_columns(); ++j) {
+            m2[i][j] = i * j * 2;
+        }
+    }
+    m1 *= 2;
+    assert(m1 == m2);
+    assert(!(m1 != m2));
+    assert(m1.get_rows() == rows);
+    assert(m1.get_columns() == cols);
+
+    m1[3][3] = 55;
+    int tmp = m1[3][3];
+    assert(tmp == 55);
+    std::cout << "Tests passed!" << std::endl;
+    return 0;
+}
